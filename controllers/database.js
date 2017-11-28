49d5f57238e3b72f4862d5fa0d0b43921a630658
RenatoRiveraJr <renato.aspuria.rivera@gmail.com>
2017-11-28 03:19:03 -0800

    Testing collection insertion

--- controllers/database.js
+++ controllers/database.js
@@ -6,10 +6,29 @@ var url = "mongodb://r39rivera:Holleriv12@ds251245.mlab.com:51245/heroku_1jbzqct
 
 module.exports.storeData =  function (request, response) {
 
+    var CUSTOMERS = db.collection('CUSTOMERS');
+    var customerID = Math.floor((Math.random() * 1000000000000) + 1);
+
     MongoClient.connect(url, function(err, db) {
         if (err) throw err;
-        db.collection('ORDERS').find().toArray(function(err, docs) {
-            response.render('index', {results: docs});
+
+        var customerdata = {
+            _id: customerID,
+            FIRSTNAME: request.param('inputFirstName'),
+            LASTNAME: request.param('inputLastName'),
+            STREET: request.param('inputAddress') + ' ' + request.param('inputAddress2'),
+            CITY: request.param('inputCity'),
+            STATE: request.param('inputState'),
+            ZIP: request.param('inputZip'),
+            EMAIL: request.param('inputEmail4')
+        };
+
+        CUSTOMERS.insertOne(customerdata, function (err, result) {
+            if (err) throw err;
         });
+
+        /*db.collection('ORDERS').find().toArray(function(err, docs) {
+            response.render('storeData', {results: docs});
+        });*/
     });
 };

60e126361c77fea9c0cfc98793e2532ff2d78b50
RenatoRiveraJr <renato.aspuria.rivera@gmail.com>
2017-11-30 00:33:43 -0800

    Cleaned up Routes

--- routes/index.js
+++ routes/index.js
@@ -2,47 +2,8 @@ var express = require('express');
 var router = express.Router();
 var ControllerDatabase = require('../controllers/database');
 /* GET home page. */
-router.post('/herokuConnect', function(req, res, next) {
-   res.render('index', { title: 'Express'});
-});
-
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express'});
-});
-
-router.get('/mLabConnect', function(req, res, next) {
-
-    var MongoClient = require('mongodb').MongoClient;
-    var url = "mongodb://r39rivera:Holleriv12@ds251245.mlab.com:51245/heroku_1jbzqctc";
-
-    MongoClient.connect(url, function(err, db) {
-        if (err) throw err;
-        db.collection("ORDERS").find({name: "Renato"}).toArray(function (err, docs) {
-            if (err) throw err;
-            docs.forEach(function (doc) {
-                res.send(doc['name'] + " " + doc['order']);
-            });
-        });
-    });
-});
 
 router.post('/storeData', ControllerDatabase.storeData);
 
-/*
-router.post('/', function(req, res){
-    var MongoClient = require('mongodb').MongoClient;
-    var url = "mongodb://r39rivera:Holleriv12@ds251245.mlab.com:51245/heroku_1jbzqctc";
-
-
-    MongoClient.connect(url, function(err, db) {
-        if (err) throw err;
-        db.collection("ORDERS").find({name: req.param('username')}).toArray(function (err, docs) {
-            if (err) throw err;
-            docs.forEach(function (doc) {
-                res.send(doc['name'] + " " + doc['order']);
-            });
-        });
-    });
-});*/
 
 module.exports = router;
